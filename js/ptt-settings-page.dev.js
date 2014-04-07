03f298ddad212f5cb35b1916325893b3b8afc941
Eric Mann <eric@eamann.com>
2014-04-07 16:03:16 -0700

    Update Chosen CSS as well

--- js/ptt-settings-page.dev.js
+++ js/ptt-settings-page.dev.js
@@ -1,22 +1,24 @@
-/*global jQuery, document */
-jQuery( document ).ready( function ( $ ) {
+/*global jQuery */
+(function ( window, $, undefined ) {
+	var document = window.document;
+
 	// Adds another row to list of category/twitter account pairings
-	$( '.ptt-add-another' ).on( 'click', function() {
+	$( '.ptt-add-another' ).on( 'click', function () {
 		var $new_row = $( '#ptt-twitter-category-pairing-clone' ).clone(),
 			$div = $( '#ptt-twitter-category-pairings' );
 
 		$new_row.css( {
 			visibility : 'visible',
-			height : ''
+			height     : ''
 		} ).appendTo( $div );
 	} );
 
 	// Removes a pairing row
-	$( '.ptt-delete' ).on( 'click', function() {
+	$( '.ptt-delete' ).on( 'click', function () {
 		var $this = $( this ),
 			$parent_div = $this.parents( '.ptt-twitter-category-pairing' );
 
-		$parent_div.fadeOut( 'fast', function() {
+		$parent_div.fadeOut( 'fast', function () {
 			$parent_div.remove();
 		} );
 		return false;
@@ -24,11 +26,11 @@ jQuery( document ).ready( function ( $ ) {
 
 	// Setup Chosen for Terms
 	$( '.ptt-chosen-terms' ).chosen( {
-		no_results_text: 'No terms match'
+		no_results_text : 'No terms match'
 	} );
 
 	// Setup Chosen for Accounts
 	$( '.ptt-chosen-accounts' ).chosen( {
-		no_results_text: 'No accounts match'
+		no_results_text : 'No accounts match'
 	} );
-} );
+})( this, jQuery );
